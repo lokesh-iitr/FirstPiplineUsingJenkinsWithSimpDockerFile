bcaa7957d5b9342dab0cde8a4cf99683fca8f99e
lokesh-iitr <lokesh.mahala@quantiphi.com>
2018-07-20 18:18:55 +0530

    some change in code

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Quantiphi ISC\n');
+  res.send('Hello Quantiphi ISC lokesh\n');
 });
 
 app.listen(PORT, HOST);
