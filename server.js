423491c938b2f9802492c558dda1ad1e7e6ed703
lokesh-iitr <lokesh.mahala@quantiphi.com>
2018-07-20 06:20:39 +0530

    Third commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Quantiphi\n');
+  res.send('Hello Quantiphi ISC\n');
 });
 
 app.listen(PORT, HOST);
